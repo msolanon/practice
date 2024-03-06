5c63e5a48ffcb068145c6bd0f005530f7b840953
Michael Solano <msolanon1994@gmail.com>
2024-03-05 21:38:38 -0600

    first smart contract view

---
+++ blockchain/migrations/1_migration.js
@@ -0,0 +1,8 @@
+var MyContract = artifacts.require("SimpleVoting");
+
+module.exports = async function(deployer) {
+    // deploy a contract
+    await deployer.deploy(MyContract);
+    //access information about your deployed contract instance
+    const instance = await MyContract.deployed();
+};
