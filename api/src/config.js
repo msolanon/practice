91dfaf6c5ea3104615584b02702639e1ce05560a
Michael Solano <msolanon1994@gmail.com>
2025-12-15 17:55:54 -0600

    Revert "https"
    This reverts commit e0db49dadbd12e19e0c935f85466cc255b58f952.

--- api/src/config.js
+++ api/src/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     uri: "mongodb://127.0.0.1:27017/test",
-    NODE_URL: "http://127.0.0.1:9545"
+    NODE_URL: "http://127.0.0.1:9545",
+    CLIENT_ID: '',
+    CLIENT_SECRET: '',
+    REDIRECT_URI: '',
+    REFRESH_TOKEN: ''
+
 };
